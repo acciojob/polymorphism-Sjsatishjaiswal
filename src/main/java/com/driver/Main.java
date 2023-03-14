b9c05e0bdce6d4f0976b88247a6b480b7c9e65db
Satish Jaiswal <Sjsatishjaiswalcse2017@gmail.com>
2023-03-14 17:05:07 +0530

    Modified

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,24 +4,24 @@ public class Main {
     public static void main(String[] args) {
 
 
-        product p = new product();
-        int ans = p.product(10, 20);
-        int ans1 = p.product(10, 20, 30);
-        double ans2 = p.product(10.5, 9.7);
+        Product p = new Product();
+        int ans = p.Product(10, 20);
+        int ans1 = p.Product(10, 20, 30);
+        double ans2 = p.Product(10.5, 9.7);
         System.out.println(ans + " " + ans1 + " " + ans2);
 
     }
 
 
 
-     public static class product {
-          public int product(int x,int y){
+     public static class Product {
+          public int Product(int x,int y){
         return x*y;
     }
-    public int product(int x,int y,int z){
+    public int Product(int x,int y,int z){
         return x*y*z;
     }
-    public double product(double x,double y){
+    public double Product(double x,double y){
         return x*y;
     }
 
